38f1baa40862a4a0dea0ff0dcb88e8e2b1ab164f
xiebojie <xiebojie@qq.com>
2017-08-19 23:33:41 +0800

    delete unused

--- www/index.php
+++ www/index.php
@@ -12,7 +12,7 @@ include BASEPATH.'/daemon/uboot.inc.php';
 
 function uri_route()
 {
-    $uri = parse_url(preg_replace("/\/lvs\/usertrustnew/", '', $_SERVER['REQUEST_URI']),PHP_URL_PATH);
+    $uri = parse_url( $_SERVER['REQUEST_URI'],PHP_URL_PATH);
     $segements = explode('/', trim($uri,'/'));
     $dir = '';
     while(true)
@@ -105,32 +105,6 @@ function redirect($url)
     }
 }
 
-//@todo 仅支持term和range匹配
-function es_filter_parse($field_list, $data)
-{
-    $term = array();
-    $range = array();
-    foreach ($field_list as $_field => $_str)
-    {
-        if (isset($data[$_field]) && is_string($data[$_field]) && $data[$_field] != '')
-        {
-            $token = array('column'=>'','compare'=>array());
-            parse_str($_str, $token);
-            if($token['compare']=='term')
-            {
-                $term[] = array('term'=>array($token['column']=>$data[$_field]));
-            }elseif ($token['compare']=='date_start') 
-            {
-                $range[$token['column']]['range'][$token['column']]['from']=date('Y-m-d H:i:s',  strtotime($data[$_field]));
-            }else if($token['compare']=='date_end')
-            {
-                $range[$token['column']]['range'][$token['column']]['to']= date('Y-m-d H:i:s',  strtotime($data[$_field].' 23:59:59'));
-            }
-        }
-    }
-    return array_merge($term,  array_values($range));
-}
-
 function str_replace_star($str,$star_len=3)
 {
     $len = mb_strlen($str);
@@ -139,61 +113,6 @@ function str_replace_star($str,$star_len=3)
         return mb_ereg_replace(mb_substr($str, ceil(($len-$star_len)/2), $star_len), str_pad('', $star_len,'*'),$str);
     }
     return $str;
-}
-
-function ldap_login( $Flag = 'http')
-{
-    static $D_URL = 'https://login.ops.qihoo.net:4436/sec/login';
-    static $B_URL = 'https://tool4.ops.dxt.qihoo.net:4436/sec/login';
-    //判断D_URL是否有效,无效换备份跳转地址B_URL
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, $D_URL);
-    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1); // allow redirects 
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); // return into a variable 
-    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
-    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE);
-    curl_exec($ch);
-    $http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-    curl_close($ch);
-    if ($http_code != 200)
-    {
-        $D_URL = $B_URL;
-    }
-    //跳转
-    $sid = isset($_GET['sid']) ? $_GET['sid'] : '';
-    $ref = isset($_GET['ref']) ? $_GET['ref'] : '';
-    //获取sid
-    if (!(preg_match("/[0-9a-z]{32}/",$sid)))
-    {
-        $S_URL = $Flag . '://' . $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"];
-        $ref = $S_URL;
-        $d_url = $D_URL . '?ref=' . $S_URL;
-        redirect($d_url);
-    }else
-    {
-        if (empty($ref))
-        {
-            $S_URL = $Flag . '://' . $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"];
-            $ref = $S_URL;
-        }
-        $url = $D_URL . '?sid=' . $sid . '&ref=' . $ref;
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1); // allow redirects 
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); // return into a variable 
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE);
-        curl_setopt($ch, CURLOPT_TIMEOUT, 25);
-        $result = curl_exec($ch);
-        curl_close($ch);
-
-        $userinfo = array();
-        if ($result != 'None')
-        {
-            $userinfo = json_decode($result, true);
-        }
-        return $userinfo;
-    }
 }
 
 /***********************************初始化控制器并执行该方法*******************************************************/
